7d2a859a1643f36f10f3a5cf280bd19684271049
zeyglitch <jonniaux.math@gmail.com>
2025-10-17 14:39:03 +0200

    commit 3

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Bonjour, monde !");
+        System.out.println("Hello world!");
+        System.out.println("Tu vas bien ?");
         System.out.println("Goodbye, world!");
     }
 }
