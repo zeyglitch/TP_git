e7e9b43d3168e02c38e447c0fd9ea3e4db7db3ff
maxence81 <maxence.dabrowski@etud.univ-jfc.fr>
2025-10-17 14:48:44 +0200

    maxence first commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,11 @@ public class HelloWorld {
         System.out.println("Hello world!");
         System.out.println("Tu vas bien ?");
         System.out.println("Goodbye, world!");
+        //print ascii art cat
+        System.out.println(" /\\_/\\");
+        System.out.println("( o.o )");
+        System.out.println(" > ^ <");
     }
+
+    
 }
